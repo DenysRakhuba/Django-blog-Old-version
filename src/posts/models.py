44f1afc552d28c74e24862f3d91254072f6da78e
Denis Rakhuba <denis.rakhuba@gmail.com>
2016-06-08 21:24:28 +0300

    ..

--- src/posts/models.py
+++ src/posts/models.py
@@ -6,8 +6,10 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
-
+from django.utils.safestring import mark_safe
 from django.utils.text import slugify
+
+from markdown_deux import markdown 
 
 class PostManager(models.Manager):
     def active(self, *args, **kwargs):
@@ -45,6 +47,11 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('posts:detail', kwargs={'slug': self.slug})
+
+    def get_markdown(self):
+        content = self.content
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
 
 
 def create_slug(instance, new_slug=None):
